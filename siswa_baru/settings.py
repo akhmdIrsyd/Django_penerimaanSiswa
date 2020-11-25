ac1802a8df61a5bef84d083ab030efc1d3bec10a
irsyad <>
2020-11-25 14:05:15 +0700

    reupload

--- siswa_baru/settings.py
+++ siswa_baru/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '433(ffkqmmknkwvpsepas3$0p*1j1gpni(b0!hr=*ljjpl)&zw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -80,18 +80,9 @@ WSGI_APPLICATION = 'siswa_baru.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'db_siswa',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'admin',
-
-        'HOST': '127.0.0.1',
-
-        'PORT': '5432',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
